f53b4566804ba1ca200f01cdde7af63832acbd1d
Renata Deodório <renatadeod@gmail.com>
2023-01-09 17:38:15 -0300

    Subrotinas (function)

--- banco.php
+++ banco.php
@@ -1,5 +1,9 @@
 <?php
 
+function exibeMensagem($mensagem){
+  echo $mensagem . PHP_EOL;
+}
+
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Maria',
@@ -16,11 +20,17 @@ $contasCorrentes = [
 ];
 
 
+if (500 > $contasCorrentes['123.456.789-10']['saldo']) {
+  exibeMensagem("Você não pode sacar este valor");
+} else
+  $contasCorrentes['123.456.789-10']['saldo'] -= 500;
+
+
 if (500 > $contasCorrentes['123.456.789-11']['saldo']) {
-  echo "Você não pode sacar este valor" . PHP_EOL;
-} else 
+  exibeMensagem("Você não pode sacar este valor");
+} else
   $contasCorrentes['123.456.789-11']['saldo'] -= 500;
 
 foreach ($contasCorrentes as $cpf => $conta) {
-  echo $cpf . " " . $conta['titular'] . ' ' . $conta['saldo'] . PHP_EOL;
+  exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
 }
